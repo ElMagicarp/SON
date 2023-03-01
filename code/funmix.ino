9f31d3adb503632d1df1fb8302c371c966439a90
amFarwati <101759587+ElMagicarp@users.noreply.github.com>
2023-03-01 11:28:31 +0100

    Update funmix.ino

--- code/funmix.ino
+++ code/funmix.ino
@@ -1,7 +1,7 @@
 #include <Audio.h>
-#include "Funmix.h"
+#include "FunMix.h"
 
-Funmix myDsp;
+FunMix myDsp;
 AudioInputI2S in;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
@@ -16,40 +16,35 @@ void setup() {
   audioShield.enable();
   audioShield.inputSelect(AUDIO_INPUT_LINEIN);
   audioShield.micGain(10); // in dB
-  audioShield.volume(0.8);
+  audioShield.volume(1);
   myDsp.setParamValue("volume",0.8);
   myDsp.setParamValue("high",0);
   myDsp.setParamValue("low",0);
   myDsp.setParamValue("shift",0);
-  myDsp.setParamValue("echoDelay",70);
-  myDsp.setParamValue("echoFeedback",0.5);
+  myDsp.setParamValue("echoDelay",0);
+  myDsp.setParamValue("echoFeedback",0);
 }
 
 void loop() {
   // A0, 2, 3, 8, 17, 16, 15 , 14 are free
   float vol = (float)analogRead(A17)/1023.0; // 0-1
-  float hi = (float)analogRead(A12)*40.0/1023.0; // 0-40 dB
-  float lo = (float)analogRead(A11)*40.0/1023.0; // 0-40 dB
-  float sh = ((float)analogRead(A10)*40.0/1023.0) - 20.0; // (-20) - 20
-  float del = (float)analogRead(A16)*1000.0/1023.0; // 0-1000 ms
-  float feed = (float)analogRead(A13)/1023.0; // 0-1
-  
+  float hi = ((float)analogRead(A12)*40.0/1023.0)-20;
+  float lo = ((float)analogRead(A10)*40.0/1023.0)-20; 
+  float sh = ((float)analogRead(A14)*26.0/1023.0) - 12.0;
+  float del = (float)analogRead(A15)*800.0/1023.0; // 0-1000 ms
+  float feed = (float)analogRead(A16)*0.8/1023.0;
+  if(del<10){
+    del=0;
+    feed = 0;
+  }
   myDsp.setParamValue("volume",vol);
   myDsp.setParamValue("high",hi);
   myDsp.setParamValue("low",lo);
   myDsp.setParamValue("echoDelay",del);
   myDsp.setParamValue("echoFeedback",feed);
   myDsp.setParamValue("shift",sh);
-  Serial.print(analogRead(A10));
+  Serial.print(del);
   Serial.print(" ");
-  Serial.print(analogRead(A11));
-  Serial.print(" ");
-  Serial.print(analogRead(A12));
-  Serial.print(" ");
-  Serial.print(analogRead(A13));
-  Serial.print(" ");
-  Serial.print(analogRead(A17));
-  Serial.print(" ");
-  Serial.println(analogRead(A16));
+  Serial.println(feed);
   delay(50);
 }
